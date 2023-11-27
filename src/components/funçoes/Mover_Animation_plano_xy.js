89f5a9f6a814109fd1f1560051b43243dbd3affd
GabrielAzevedo12 <114695658+GabrielAzevedo12@users.noreply.github.com>
2023-11-27 16:51:33 +0000

    ...

--- src/components/funçoes/Mover_Animation_plano_xy.js
+++ src/components/funçoes/Mover_Animation_plano_xy.js
@@ -1,23 +1,33 @@
-let Mover_Animation_plano_xy = (elemento, limite, transformarx, transformary) => {
+import { posiçaoRelativa_x } from './posiçao_relativa_x.js';
+import { posiçaoRelativa_y } from './posiçao_relativa_y.js';
+
+let Mover_Animation_plano_xy = (elemento, limite, transformarx, transformary, Action_in_xy) => {
 
     let x = posiçaoRelativa_x(elemento);
+    let y = posiçaoRelativa_y(elemento);
+    let Interval_for_Action = setInterval(() => {
+        Mover_Animation_plano_xy(elemento, limite, transformarx, transformary, Action_in_xy);
+    },0);
+
     if (x >= limite) {
 
-        clearInterval(Desloc_interval);
+        clearInterval(Interval_for_Action);
 
     } else {
 
         x = transformarx(x);
         y = transformary(x);
-        Deslocar_plano_xy(elemento, x, y);
+        Action_in_xy(elemento, x, y);
 
     } 
 }
 
 /*
-let Desloc_interval = setInterval(() => {
+let Interval_for_Action = setInterval(() => {
         Desloc(fgs.$("#retaClass_horizontal"),1000,Atualizar)
     },0);
+
+Deslocar_plano_xy(elemento, x, y);    
  */
 
 export { Mover_Animation_plano_xy }
