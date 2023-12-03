4bd1fd3d4e7b83eee3e45d26992a9d83fcdc286b
GabrielAzevedo12 <114695658+GabrielAzevedo12@users.noreply.github.com>
2023-12-03 04:32:29 +0000

    ...

--- src/components/Layout/Text/Text.js
+++ src/components/Layout/Text/Text.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './css/Text.css';
+import styled from "styled-components";
 //import styled, {keyframes} from "styled-components";
 
 const Text = ( props ) => { 
@@ -7,18 +8,34 @@ const Text = ( props ) => {
     className =`${props.TextName}_Container Text center ${props.className || ""}`,
     id = `${props.TextName}`;
 
+    
+    const TextTitulo = styled.h1`
+    text-align: center;
+    font-size: 4rem;
+    color: rgb(24, 24, 24);
+    font-family: cursive;`
+
+    const TextParagrafo = styled.h1`
+    text-align: center;
+    font-size: 1.2rem;
+    color: rgb(38, 54, 54);`
+
     const analise_typeText = (typeText) => {
         if(typeText === "h1") {
             return (
-                <h1>
-                    {props.children}
-                </h1>)
+                <TextTitulo>
+                    <h1>
+                        {props.children}
+                    </h1>   
+                </TextTitulo>)
         }
         if(typeText === "p") {
             return (
-                <p>
-                    {props.children}
-                </p>)
+                <TextParagrafo>
+                    <p>
+                        {props.children}
+                    </p>
+                </TextParagrafo>)
         }
     }
 
