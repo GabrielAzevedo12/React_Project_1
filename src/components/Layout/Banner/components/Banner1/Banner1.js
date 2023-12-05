56553ab43e7294849729a650b12ca2c870be24aa
GabrielAzevedo12 <114695658+GabrielAzevedo12@users.noreply.github.com>
2023-12-05 18:58:31 +0000

    ...

--- src/components/Layout/Banner/components/Banner1/Banner1.js
+++ src/components/Layout/Banner/components/Banner1/Banner1.js
@@ -1,12 +1,11 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer } from "react";
 import BannerGeral from "../../BannerGeral/BannerGeral";
 import SubBanner from "../../subBanner/subBanner";
-//import Text from "../../../Text/Text";
 import Text from "../../../Text/Text";
 import { AiOutlineLoading } from "react-icons/ai";
 import { MovLeft_subBanner } from "./css/MovLeft_subBanner.css.js"
 import { rotate } from "./css/rotate.css.js"
-import styled, {keyframes} from "styled-components";
+import styled, { css } from "styled-components";
 import './css/Banner1.css';
 import './css/Banner1_subBanner1.css'
 import './css/Banner1_subBanner2.css'
@@ -15,12 +14,30 @@ import './css/Banner1_subBanner3.css'
 const reducer = (stateAtual, stateUp) => {
   return { ...stateAtual, ...stateUp }
 }
+
+let StyledSubBanner = 
+  styled(SubBanner)`
+  position: relative;
+  animation:
+  ${
+  props => props.animation && !props.OverSubBanner ? 
+  css`${props.animation} 90s linear infinite ;` :
+  `` 
+  }
+  `
+
+const StyledText = styled(Text)`
+  position: relative;
+  animation: ${rotate} 2s cubic-bezier(.79,2.01,.83,.67) infinite;
+  `;
+
 const Banner1 = () => { 
 
   const [states, updateStates ] = useReducer(reducer, {
     subBanner1_left: false,
     subBanner2_left: false,
     subBanner3_left: false,
+    subBanner_animation: MovLeft_subBanner,
     OverSubBanner: false,
     loadText: false
   })
@@ -29,25 +46,7 @@ const Banner1 = () => {
     updateStates({ loadText: true })
   }, 5000);
 
-   const StyledText = styled(Text)`
-   position: relative;
-   animation: ${rotate} 2s cubic-bezier(.79,2.01,.83,.67) infinite;
-   padding: 2rem 1rem;
-   font-size: 1.2rem;
-   `;
-
-   let StyledSubBanner;
-
-   states.OverSubBanner ? 
-
-   StyledSubBanner = styled(SubBanner)`
-   position: relative;
-   ` :
-   
-   StyledSubBanner = styled(SubBanner)`
-   position: relative;
-   animation: ${MovLeft_subBanner} 90s cubic-bezier(.79,2.01,.83,.67) infinite ;
-   `
+   /*animation: ${MovLeft_subBanner} 90s cubic-bezier(.79,2.01,.83,.67) infinite ;*/
    const SubBanner_onMouseOver = (e) => {
     for ( const element of e.target.parentElement.childNodes) {
     var
@@ -61,7 +60,13 @@ const Banner1 = () => {
 
     return (
       <BannerGeral BannerName="Banner1" className="flex-row flex-nowrap">
-        <StyledSubBanner BannerPai="Banner1" indexItem={1} className="center" onMouseOver={SubBanner_onMouseOver}>
+        <StyledSubBanner 
+        BannerPai="Banner1" 
+        indexItem={1} 
+        className="center" 
+        onMouseOver={SubBanner_onMouseOver} 
+        animation={states.subBanner_animation}
+        OverSubBanner={states.OverSubBanner}>
         {
                 states.loadText ?
                 <Text TextName="subBanner1_Text" typeText="h1"> 
@@ -72,7 +77,13 @@ const Banner1 = () => {
                 </StyledText>
         }
         </StyledSubBanner>
-        <StyledSubBanner BannerPai="Banner1" indexItem={2} className="" onMouseOver={SubBanner_onMouseOver}>
+        <StyledSubBanner 
+        BannerPai="Banner1" 
+        indexItem={2} 
+        className="" 
+        onMouseOver={SubBanner_onMouseOver} 
+        animation={states.subBanner_animation}
+        OverSubBanner={states.OverSubBanner}>
         {
                 states.loadText ?
                 <Text TextName="subBanner1_Text" typeText="h1"> 
@@ -83,7 +94,13 @@ const Banner1 = () => {
                 </StyledText>
         }
         </StyledSubBanner>
-        <StyledSubBanner BannerPai="Banner1" indexItem={3} className="" onMouseOver={SubBanner_onMouseOver}>
+        <StyledSubBanner 
+        BannerPai="Banner1" 
+        indexItem={3} 
+        className="" 
+        onMouseOver={SubBanner_onMouseOver} 
+        animation={states.subBanner_animation}
+        OverSubBanner={states.OverSubBanner}>
         {
                 states.loadText ?
                 <Text TextName="subBanner1_Text" typeText="h1"> 
--- src/components/Layout/Text/Text.js
+++ src/components/Layout/Text/Text.js
@@ -3,22 +3,21 @@ import './css/Text.css';
 import styled from "styled-components";
 //import styled, {keyframes} from "styled-components";
 
+const TextTitulo = styled.h1`
+text-align: center;
+font-size: 4rem;
+color: ${ (props) => props.color || "rgb(24, 24, 24)" } ;
+font-family: cursive;`
+
+const TextParagrafo = styled.p`
+text-align: center;
+font-size:  ${ (props) => props.fontSize || "1.2rem" } ;
+color: ${ (props) => props.color || "rgb(38, 54, 54)" } ;`
+
 const Text = ( props ) => { 
     const 
     className =`${props.TextName}_Container Text center ${props.className || ""}`,
     id = `${props.TextName}`;
-
-    
-    const TextTitulo = styled.h1`
-    text-align: center;
-    font-size: 4rem;
-    color: ${ (props) => props.color || "rgb(24, 24, 24)" } ;
-    font-family: cursive;`
-
-    const TextParagrafo = styled.p`
-    text-align: center;
-    font-size:  ${ (props) => props.fontSize || "1.2rem" } ;
-    color: ${ (props) => props.color || "rgb(38, 54, 54)" } ;`
 
     const analise_typeText = (typeText) => {
         if(typeText === "h1") {
