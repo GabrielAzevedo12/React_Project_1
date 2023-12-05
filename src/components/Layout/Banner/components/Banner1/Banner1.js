70a401d799d94109fe01f09986ff8b938e16e435
GabrielAzevedo12 <114695658+GabrielAzevedo12@users.noreply.github.com>
2023-12-05 17:52:27 +0000

    ...

--- src/components/Layout/Banner/components/Banner1/Banner1.js
+++ src/components/Layout/Banner/components/Banner1/Banner1.js
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import BannerGeral from "../../BannerGeral/BannerGeral";
 import SubBanner from "../../subBanner/subBanner";
 //import Text from "../../../Text/Text";
 import Text from "../../../Text/Text";
 import { AiOutlineLoading } from "react-icons/ai";
 import { MovLeft_subBanner } from "./css/MovLeft_subBanner.css.js"
+import { rotate } from "./css/rotate.css.js"
 import styled, {keyframes} from "styled-components";
 import './css/Banner1.css';
 import './css/Banner1_subBanner1.css'
 import './css/Banner1_subBanner2.css'
 import './css/Banner1_subBanner3.css'
 
+const reducer = (stateAtual, stateUp) => {
+  return { ...stateAtual, ...stateUp }
+}
 const Banner1 = () => { 
 
-  const [OverSubBanner, setOverSubBanner] = useState(false);
-  const [loadText, setloadText] = useState(false);
-  setTimeout(() => {
-    setloadText(true)
-  }, 10000);
-   // Create the keyframes
-   const rotate = keyframes`
-   from {
-   transform: rotate(0deg);
-   }
+  const [states, updateStates ] = useReducer(reducer, {
+    subBanner1_left: false,
+    subBanner2_left: false,
+    subBanner3_left: false,
+    OverSubBanner: false,
+    loadText: false
+  })
 
-   to {
-   transform: rotate(360deg);
-   }
-   `;
+  setTimeout(() => {
+    updateStates({ loadText: true })
+  }, 5000);
 
    const StyledText = styled(Text)`
    position: relative;
@@ -38,23 +38,32 @@ const Banner1 = () => {
 
    let StyledSubBanner;
 
-   OverSubBanner ? 
+   states.OverSubBanner ? 
 
    StyledSubBanner = styled(SubBanner)`
    position: relative;
-   animation: unset ;
    ` :
    
    StyledSubBanner = styled(SubBanner)`
    position: relative;
    animation: ${MovLeft_subBanner} 90s cubic-bezier(.79,2.01,.83,.67) infinite ;
    `
+   const SubBanner_onMouseOver = (e) => {
+    for ( const element of e.target.parentElement.childNodes) {
+    var
+    d_e = element.getBoundingClientRect(),
+    d_p = element.parentElement.getBoundingClientRect() ;
+    element.style.left = (d_p.x - d_e.x) + "px" ;
+
+    }
+    updateStates({OverSubBanner: true})
+   }
 
     return (
       <BannerGeral BannerName="Banner1" className="flex-row flex-nowrap">
-        <StyledSubBanner BannerPai="Banner1" indexItem={1} className="center" OverSubBanner={OverSubBanner} onMouseOver={() => setOverSubBanner(true)}>
+        <StyledSubBanner BannerPai="Banner1" indexItem={1} className="center" onMouseOver={SubBanner_onMouseOver}>
         {
-                loadText ?
+                states.loadText ?
                 <Text TextName="subBanner1_Text" typeText="h1"> 
                 subBannner 1
                 </Text> :
@@ -63,9 +72,9 @@ const Banner1 = () => {
                 </StyledText>
         }
         </StyledSubBanner>
-        <StyledSubBanner BannerPai="Banner1" indexItem={2} className="" OverSubBanner={OverSubBanner} onMouseOver={() => setOverSubBanner(true)}>
+        <StyledSubBanner BannerPai="Banner1" indexItem={2} className="" onMouseOver={SubBanner_onMouseOver}>
         {
-                loadText ?
+                states.loadText ?
                 <Text TextName="subBanner1_Text" typeText="h1"> 
                 subBannner 2
                 </Text> :
@@ -74,9 +83,9 @@ const Banner1 = () => {
                 </StyledText>
         }
         </StyledSubBanner>
-        <StyledSubBanner BannerPai="Banner1" indexItem={3} className="" OverSubBanner={OverSubBanner} onMouseOver={() => setOverSubBanner(true)}>
+        <StyledSubBanner BannerPai="Banner1" indexItem={3} className="" onMouseOver={SubBanner_onMouseOver}>
         {
-                loadText ?
+                states.loadText ?
                 <Text TextName="subBanner1_Text" typeText="h1"> 
                 subBannner 3
                 </Text> :
